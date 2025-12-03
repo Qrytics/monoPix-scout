aec37eed11fa949454cf3739b5214cd011e37128
Mario Belmonte <118155385+Qrytics@users.noreply.github.com>
2025-12-03 09:53:33 -0500

    Update content.js
    Changes:
    - Replace inline script injection with external script reference.

--- extension/content.js
+++ extension/content.js
@@ -70,38 +70,8 @@ chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
 });
 
 (function injectHooksIntoPage() {
-  const code = `
-    (function() {
-      const _beacon = navigator.sendBeacon && navigator.sendBeacon.bind(navigator);
-      if (_beacon) {
-        navigator.sendBeacon = function(url, data) {
-          try {
-            window.postMessage(
-              { source: "monopix", kind: "beacon", url: String(url), reason: "navigator.sendBeacon" },
-              "*"
-            );
-          } catch (e) {}
-          return _beacon(url, data);
-        };
-      }
-
-      const _fetch = window.fetch && window.fetch.bind(window);
-      if (_fetch) {
-        window.fetch = function(input, init) {
-          try {
-            window.postMessage(
-              { source: "monopix", kind: "xhr", url: String(input), reason: "fetch" },
-              "*"
-            );
-          } catch (e) {}
-          return _fetch(input, init);
-        };
-      }
-    })();
-  `;
-
   const s = document.createElement("script");
-  s.textContent = code;
+  s.src = chrome.runtime.getURL("page_hooks.js");
   (document.head || document.documentElement).appendChild(s);
   s.remove();
 })();
